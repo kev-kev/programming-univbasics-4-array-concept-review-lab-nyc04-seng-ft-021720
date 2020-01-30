f90dce34a98b8471ad7fb94c4f1ad2097038fc98
Kevin Clark <kevin.clark93@gmail.com>
2020-01-30 23:47:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,13 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  #can't just sort and do array[-1]
+  #need to iterate over the array and evaluate at each index and see if the value is greater than the index before
+  array.length.time do |i|
+    if array[i]
+      
+    end
+  end
 end
 
 

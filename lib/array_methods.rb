d1d23c8e2b418d90e98790652d2358b95522ae54
Kevin Clark <kevin.clark93@gmail.com>
2020-01-30 23:27:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,13 @@
 def find_element_index(array, value_to_find)
-  i = 0
-  if array[i] === value_to_find
-    return i
-  else
-    i ++
+  length = array.length
+  length.times do |i|
+    if array[i] === value_to_find
+      return i
+    else
+      i ++
+    end
+    nil
   end
-  nil
-end
 
 
 def find_max_value(array)

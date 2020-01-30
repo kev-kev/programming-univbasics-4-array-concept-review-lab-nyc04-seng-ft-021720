f79aa8475eb8788f25bbbad337eadeb07c69c38e
Kevin Clark <kevin.clark93@gmail.com>
2020-01-30 23:58:21 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,33 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do |i|
+    if array[i] == value_to_find
+      return i
+    end
+  end
+  nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  #can't just sort and do array[-1]
+  #need to iterate over the array and evaluate at each index and see if the value is greater than the index before
+  x = 0
+  array.length.times { |i|
+    if array[i] > x
+      x = array[i]
+    end
+  }
+  x
 end
 
+
 def find_min_value(array)
-  # Add your solution here
+  #can take the find_max_value method and switch the sign
+  #if we do that, we need to change the value of x, since 0 would be less than any element in an array of integers.
+  x = array[0]
+  array.length.times { |i|
+    if array[i] < x
+      x = array[i]
+    end
+  }
+  x
 end

46ca9edd2394233b4e9507a0f798f697371e4e50
Kevin Clark <kevin.clark93@gmail.com>
2020-01-30 23:58:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,15 +10,14 @@ end
 def find_max_value(array)
   #can't just sort and do array[-1]
   #need to iterate over the array and evaluate at each index and see if the value is greater than the index before
-  x = array[0]
+  x = 0
   array.length.times { |i|
-    if array[i] < x
+    if array[i] > x
       x = array[i]
     end
   }
   x
 end
-
 
 
 def find_min_value(array)
